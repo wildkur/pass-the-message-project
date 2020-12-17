15399b273e27f3f603678f56faed1041f669cdd5
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-17 13:18:50 +0100

    1.0

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 // Tu código aquí
-
+(function() {
 const form = document.querySelector('#message-form')
 
 form.addEventListener('submit', function(e){
     console.log("Información sobre el objeto event: ", e)
     e.preventDefault() // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada. 
-
-    console.log("Hemos hecho clic en el botón de submit!")
-
+    var inputMessage = document.querySelector("#message")
+    var alertMessage = document.querySelector(".feedback")
+    var displayMessage = document.querySelector(".message-content")
+    
+    if (inputMessage === '') {
+        alertMessage.classList.add('show')
+        //setTimeout(function(){alertMessage.classList.remove('show')}, 2000)
+    }
+    else{
+        displayMessage.textContent = inputMessage.value
+        console.log("Display that:" + displayMessage.textContent)
+        inputMessage.value = ''
+    }
+    
 })
+})()
+
+
